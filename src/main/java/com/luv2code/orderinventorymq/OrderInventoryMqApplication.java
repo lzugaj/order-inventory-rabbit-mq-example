1ba5a7221d248b841ec6d132c58e409831821524
lzugaj <luka.zugaj@sigit.hr>
2025-02-14 16:57:22 +0100

    First commit

---
+++ src/main/java/com/luv2code/orderinventorymq/OrderInventoryMqApplication.java
@@ -0,0 +1,13 @@
+package com.luv2code.orderinventorymq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderInventoryMqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderInventoryMqApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/luv2code/orderinventorymq/OrderInventoryMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.orderinventorymq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderInventoryMqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
